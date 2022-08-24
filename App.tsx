b35fe28915701841f533fed89e7665cb833b743f
ncb6206 <ncb6206@gmail.com>
2022-08-25 02:37:39 +0900

    install react native navigation

--- App.tsx
+++ App.tsx
@@ -1,27 +1,26 @@
 import { StatusBar } from "expo-status-bar";
-import React from 'react';
-import styled from 'styled-components/native';
+import React from "react";
+import styled from "styled-components/native";
 import { Button } from "react-native";
 
 const Container = styled.View`
-  flex: 1,
-  backgroundColor: "#fff",
-  alignItems: "center",
-  justifyContent: "center",
+  flex: 1;
+  background-color: #fff;
+  align-items: center;
+  justify-content: center;
 `;
 
 const Font = styled.Text`
-  fontSize: 30,
-  marginBottom : 10,
+  font-size: 30;
+  margin-bottom: 10;
 `;
 
 export default function App() {
   return (
     <Container>
       <Font>Button Component </Font>
-      <Button title="Button" onPress={() => alert("click!")}/>
+      <Button title="Button" onPress={() => alert("click!")} />
       <StatusBar style="auto" />
     </Container>
   );
 }
-
