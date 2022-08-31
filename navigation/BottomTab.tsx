6269ebe5de11e67b83eca4e6dce82cd48c60648a
ncb6206 <ncb6206@gmail.com>
2022-09-01 02:56:42 +0900

    tab navigation icon 설정 완료

--- navigation/BottomTab.tsx
+++ navigation/BottomTab.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons, FontAwesome, Octicons } from "@expo/vector-icons";
 import Community from "../pages/Community";
 import Profile from "../pages/Profile";
 import WordExtract from "../pages/WordExtract";
@@ -14,11 +15,71 @@ export default function BottomTab() {
       initialRouteName="Main"
       screenOptions={{ headerShown: false }}
     >
-      <Tab.Screen name="Main" component={Main} />
-      <Tab.Screen name="StackDictionary" component={StackDictionary} />
-      <Tab.Screen name="Community" component={Community} />
-      <Tab.Screen name="WordExtract" component={WordExtract} />
-      <Tab.Screen name="Profile" component={Profile} />
+      <Tab.Screen
+        name="메인"
+        component={Main}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name="ios-home"
+              style={{ color: focused ? "#00B386" : "#404040" }}
+              size={24}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="검색"
+        component={StackDictionary}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <FontAwesome
+              name="search"
+              style={{ color: focused ? "#00B386" : "#404040" }}
+              size={24}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="커뮤니티"
+        component={Community}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Octicons
+              name="comment-discussion"
+              style={{ color: focused ? "#00B386" : "#404040" }}
+              size={24}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="추출"
+        component={WordExtract}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <FontAwesome
+              name="file-word-o"
+              style={{ color: focused ? "#00B386" : "#404040" }}
+              size={24}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="프로필"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name="person"
+              style={{ color: focused ? "#00B386" : "#404040" }}
+              size={24}
+            />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
